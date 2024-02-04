a66367754a9c7244e23a241a080c9fbfb80c6473
singhrahul <singhrahul10041998@gmail.com>
2024-02-04 16:11:20 +0530

    updated changes

--- src/libs/mongodb.tsx
+++ src/libs/mongodb.tsx
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
+
+
+
 const connectMongoDB = async () => {
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI );
+    await mongoose.connect(process.env.MONGODB_URI || "");
+
     console.log("Connected to MongoDB.");
+    
   } catch (error) {
     console.log(error);
   }
